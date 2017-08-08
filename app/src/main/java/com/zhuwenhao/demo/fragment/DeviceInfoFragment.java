cf395ca02b88b4ba36e470da9306f7617f6f13ff
Juno <zhuwhao@outlook.com>
2017-08-08 15:22:38 +0800

    none

--- app/src/main/java/com/zhuwenhao/demo/fragment/DeviceInfoFragment.java
+++ app/src/main/java/com/zhuwenhao/demo/fragment/DeviceInfoFragment.java
@@ -1,21 +1,51 @@
 package com.zhuwenhao.demo.fragment;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.zhuwenhao.demo.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class DeviceInfoFragment extends Fragment {
+
+    @BindView(R.id.text_model)
+    TextView textModel;
+
+    @BindView(R.id.text_manufacturer)
+    TextView textManufacturer;
+
+    @BindView(R.id.text_board)
+    TextView textBoard;
+
+    @BindView(R.id.text_device)
+    TextView textDevice;
+
+    @BindView(R.id.text_product)
+    TextView textProduct;
+
+    @BindView(R.id.text_android_version)
+    TextView textAndroidVersion;
+
+    @BindView(R.id.text_code_name)
+    TextView textCodeName;
+
+    @BindView(R.id.text_api_level)
+    TextView textApiLevel;
+
+    Unbinder unbinder;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_device_info, container, false);
-        ButterKnife.bind(this, view);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
@@ -26,6 +56,35 @@ public class DeviceInfoFragment extends Fragment {
     }
 
     private void initView() {
+        textModel.setText(Build.MODEL);
+        textManufacturer.setText(Build.MANUFACTURER);
+        textBoard.setText(Build.BOARD);
+        textDevice.setText(Build.DEVICE);
+        textProduct.setText(Build.PRODUCT);
 
+        textAndroidVersion.setText(Build.VERSION.RELEASE);
+        textCodeName.setText(getCodeName());
+        textApiLevel.setText(String.valueOf(Build.VERSION.SDK_INT));
+    }
+
+    private String getCodeName() {
+        switch (Build.VERSION.SDK_INT) {
+            case 21:
+            case 22:
+                return "Lollipop";
+            case 23:
+                return "Marshmallow";
+            case 24:
+            case 25:
+                return "Nougat";
+            default:
+                return "";
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
